7241c1dd334bf84f1d0bfae1eb4263ad09474787
kaushal <kaushalratnapagol@gmail.com>
2023-09-27 12:41:31 +0530

    MongoDb connected

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./Admin/adminRoutes')
+const env = require('./.env')
 
-// require('dotenv').config()
+require('dotenv').config()
 
 
 
 
 const app = express()
-app.use(cors())
-const PORT = process.env.PORT || 5000
+// app.use(cors())
+const PORT = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use(express.json())
 
-// mongoose
-//   .connect(process.env.MONGODB_URL)
-//   .then(() => console.log('Connected to MongoDB!'))
-//   .catch((err) => console.log(err))
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.log(err))
 
 //   const adminRoutes = require('./Admin/adminRoutes'); 
 //   app.use(adminRoutes);
