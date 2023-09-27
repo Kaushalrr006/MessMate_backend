070e86187470d0bd4039f250e230aa7c0f02f53f
kaushal <kaushalratnapagol@gmail.com>
2023-09-27 06:13:30 +0530

    Base app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./Admin/adminRoutes')
+
+// require('dotenv').config()
+
+
+
+
+const app = express()
+app.use(cors())
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+// mongoose
+//   .connect(process.env.MONGODB_URL)
+//   .then(() => console.log('Connected to MongoDB!'))
+//   .catch((err) => console.log(err))
+
+//   const adminRoutes = require('./Admin/adminRoutes'); 
+//   app.use(adminRoutes);
+
+// app.use(routes)
+
+app.listen(PORT, () => console.log('listening on port 5000'))
